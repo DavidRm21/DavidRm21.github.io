be61f2f3db6af9099cbd44f922d3d37dfafff0a1
Cristian Romero <cdavidromero214@gmail.com>
2023-12-18 18:16:33 -0500

    add conf

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://davidrm21.github.io/DavidRm21.github.io",
 })
